43a0a810fd71dfef3c420fe7ceadff7b0ae70ec4
Brendan Thompson <me@brendanthompson.com>
2019-07-08 12:33:46 +1000

    feat(azure_secret): initial commit of role schema

--- vault/resource_azure_secret_backend_role.go
+++ vault/resource_azure_secret_backend_role.go
@@ -14,7 +14,51 @@ func azureSecretBackendRoleResource() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
-		Schema: map[string]*schema.Schema{},
+		Schema: map[string]*schema.Schema{
+			"role_name": {
+				Type: schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				Description: "The name of the Vault role."
+			},
+			"azure_roles": {
+				Type: schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource {
+					Schema: map[string]*schema.Schema {
+						"role_name": {
+							Type: schema.TypeString,
+							Optional: true,
+							Description: "The case-insensitive name of the Azure role.",
+						},
+						"role_id": {
+							Type: schema.TypeString,
+							Optional: true,
+							Description: "The explicit Azure role id",
+						},
+						"scope": {
+							Type: schema.TypeString,
+							Required: true,
+							Description: "The scope of the Vault role",
+						},
+					},
+				},
+
+			},
+			"application_object_id": {
+				Type: schema.TypeString,
+				Optional: true,
+				Description: "The Application Object ID of the SP being used to provision the Vault role.",
+			},
+			"ttl": {
+				Type: schema.TypeString,
+				Optional: true,
+			},
+			"max_ttl": {
+				Type: schema.TypeString,
+				Optional: true,
+			},
+		},
 	}
 }
 
